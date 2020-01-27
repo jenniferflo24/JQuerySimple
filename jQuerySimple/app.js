6c1c108dc74fc7905df5c197a945daf34eb611ac
jenniferflo24 <59664282+jenniferflo24@users.noreply.github.com>
2020-01-27 16:59:27 -0600

    Add files via upload

---
+++ jQuerySimple/app.js
@@ -0,0 +1,15 @@
+$("#btnSubmit").click(function(){
+    alert("Help Me Code");
+    
+});
+
+$("form").submit(function(){
+    var text = $("#message").val();
+    alert(text)
+
+$("#btnSubmit").prop("disabled", true);
+});
+
+$("<div></div>").appendTo("body")
+$("<lu></lu>").appendTo("div")
+
