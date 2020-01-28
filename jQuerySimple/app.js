82c1a454b728440733e136a0dd6ca9360c0f188e
jenniferflo24 <59664282+jenniferflo24@users.noreply.github.com>
2020-01-28 15:59:19 -0600

    Add files via upload

--- jQuerySimple/app.js
+++ jQuerySimple/app.js
@@ -1,15 +1,55 @@
-$("#btnSubmit").click(function(){
+let ul = $("<ul></ul>");
+$('body').append(ul);
+
+$("#btnSubmit").click(function () {
     alert("Help Me Code");
-    
 });
 
-$("form").submit(function(){
+$("form").submit(function () {
     var text = $("#message").val();
     alert(text)
 
-$("#btnSubmit").prop("disabled", true);
+    $("#btnSubmit").prop("disabled", true);
 });
 
 $("<div></div>").appendTo("body")
-$("<lu></lu>").appendTo("div")
+$('#btnSubmit').click(function () {
+    event.preventDefault();
+    let li = $('<li></li>');
+    li.text($('#message').val());
+    ul.append(li);
+
+    $(li).click(function () {
+        $(li).css({
+            "color": getRandomColor(),
+        });
+    });
+
+    $(li).dblclick(function() {
+        $(li).remove(li);
+    });
+
+    // $('<h2>' + $('#message').val() + '</h2>').appendTo('div')
+    // $("<lu></lu>").appendTo("div")
+    // $('h2').mouseover(function () {
+    //     // $('h2').css("background", "green")
+    //     $("h2").css({
+    //         "background-color": getRandomColor(),
+    //         "border-radius": "5px"
+    //     });
+    // })
+
+
+})
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
 
